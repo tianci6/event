dd197e04cbea17a28148fed8f6eba8106898b965
tianci <fengtianci4@163.com>
2023-04-11 21:53:25 +0800

    随便吧，赶快毁灭，usm真好

--- src/main/java/com/entity/ConfigEntity.java
+++ src/main/java/com/entity/ConfigEntity.java
@@ -7,7 +7,9 @@ import com.baomidou.mybatisplus.annotations.TableName;
 import com.baomidou.mybatisplus.enums.IdType;
 
 /**
-* 类说明 : 
+* 类说明 :
+ * 有的代码写一遍好多项目都能用。如果时代发展的慢一点儿，或许我们大家就都能更幸福，更快乐一点。
+ * 冯天赐先生的一点儿感想
 */
 @TableName("config")
 public class ConfigEntity implements Serializable{
